d826b3290dc263e04ac4eb279197538e267224d0
s-AGOPAL <s-AGOPAL@484E293874.lwsd.k12wa.org>
2022-05-24 13:38:02 -0700

    yo done with schoooooool

--- Serialization/src/Program.java
+++ Serialization/src/Program.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
+import java.util.Scanner;
 
 import com.google.gson.Gson;
 
@@ -14,9 +15,14 @@ public class Program {
     // Array of Shapes to be drawn on the canvas.
     private static AllShapes _shapes = new AllShapes();
     
-    private static void loadShapes(String fileName)
+    private static void loadShapes(String fileName) throws FileNotFoundException
     {
+    	File inputFile = new File(fileName);
+    	Scanner reader = new Scanner(inputFile);
+    	String line = reader.nextLine();
     	
+    	Gson deserializer = new Gson();
+    	_shapes = deserializer.fromJson(line, AllShapes.class);
     }
     
     private static void saveShapes(String fileName) throws FileNotFoundException
@@ -37,7 +43,7 @@ public class Program {
         _canvas.open();
         
         // create a bunch of shapes
-        _shapes.createShapes();
+        loadShapes("victor.json");
         
         // draw them all on the canvas
         _shapes.drawShapes(_canvas);
@@ -49,11 +55,13 @@ public class Program {
         _shapes.drawShapes(_canvas);
         _canvas.pause();
         
-        saveShapes("victor.json");
         _canvas.clear();
         _shapes.translateShapes(15, 18);
         _shapes.drawShapes(_canvas);
         _canvas.pause();
+        
+        
+        saveShapes("victor.json");
         // close the canvas
         _canvas.close();
     }
